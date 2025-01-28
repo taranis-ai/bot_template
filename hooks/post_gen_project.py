1a146c3675f2af7dc5091f7922cee2c49c47415c
llinauer <l.linauer@gmx.at>
2025-01-28 08:51:51 +0100

    Add post generation hook; init git repo and add remote url as origin

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+import shutil
+import subprocess
+
+def init_git_repo():
+    try:
+        subprocess.run(["git", "init"], check=True)
+        subprocess.run(["git", "remote", "add", "origin", "{{cookiecutter.repo_url}}"])
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to initialize Git repository: {e}")
+
+if __name__ == "__main__":
+    if {{cookiecutter.init_git_repo}}:
+        init_git_repo()
