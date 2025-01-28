f898d72f8a374e83b055df589db338f65a8bd85b
llinauer <l.linauer@gmx.at>
2025-01-28 11:48:39 +0100

    Change generic remote repo url to github url; implement logic to use ssh instead of https in post gen hook

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ You will be prompted a few questions:
   [1/5] Project Name: AwesomeBot
   [2/5] Your E-Mail (author@ait.ac.at): Your Mail
   [3/5] Description (A short description of what the bot does): Description
-  [4/5] Repository URL (''): Github repo URL (if you want to publish repo)
+  [4/5] Github URL (''): Github repo URL (if you want to publish repo)
   [5/5] Initialize git repo? [y/n] (n):
 ```
 Cookiecutter will create a project directory with the correct structure and pre-defined configs for you.
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,7 +4,7 @@ import subprocess
 def init_git_repo():
     try:
         subprocess.run(["git", "init"], check=True)
-        subprocess.run(["git", "remote", "add", "origin", "{{cookiecutter.repo_url}}"])
+        subprocess.run(["git", "remote", "add", "origin", "{{cookiecutter.repo_url.replace('http://', 'git@').replace('https://', 'git@').replace('github.com/', 'github.com:')}}"])
         print("Added remote origin '{{cookiecutter.repo_url}}'. Please make sure this repository exists.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to initialize Git repository: {e}")
