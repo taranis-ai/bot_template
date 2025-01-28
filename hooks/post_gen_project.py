28fea2c76a02646a82827471cf441113710ab94c
llinauer <l.linauer@gmx.at>
2025-01-28 08:58:34 +0100

    Add message to user when initializing git repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,6 +5,7 @@ def init_git_repo():
     try:
         subprocess.run(["git", "init"], check=True)
         subprocess.run(["git", "remote", "add", "origin", "{{cookiecutter.repo_url}}"])
+        print("Added remote origin '{{cookiecutter.repo_url}}'. Please make sure this repository exists.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to initialize Git repository: {e}")
 
