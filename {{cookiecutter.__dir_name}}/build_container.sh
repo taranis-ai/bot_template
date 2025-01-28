7a2f931160f5f4f5fbc166be7029762f48e6b9ad
llinauer <l.linauer@gmx.at>
2025-01-28 12:20:55 +0100

    Add checks for missing uv.lock and detached HEAD to build_container.sh

--- {{cookiecutter.__dir_name}}/build_container.sh
+++ {{cookiecutter.__dir_name}}/build_container.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+if [ ! -e "uv.lock" ]
+then
+    echo "uv.lock does not exist. Please run uv sync to create it"
+    echo "Error: Cannot build container without uv.lock -> Abort"
+    exit
+fi
+
 set -eou pipefail
 
 cd $(git rev-parse --show-toplevel)
 
 GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY_OWNER:-"ghcr.io/taranis-ai"}
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
+buildx_tags="--tag ${GITHUB_REPOSITORY_OWNER}/taranis-awesomebot:latest "
 
-echo "Building containers for branch ${CURRENT_BRANCH} on ${GITHUB_REPOSITORY_OWNER}"
+if git rev-parse --quiet --verify HEAD >/dev/null; then
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
+    echo "Building image for branch ${CURRENT_BRANCH} on ${GITHUB_REPOSITORY_OWNER}"
+    buildx_tags+="--tag ${GITHUB_REPOSITORY_OWNER}/taranis-awesomebot:${CURRENT_BRANCH}"
+else
+    echo "The repository has no commits yet. Building image locally"
+fi
+
 
 docker buildx build --file Containerfile \
   --build-arg GITHUB_REPOSITORY_OWNER="${GITHUB_REPOSITORY_OWNER}" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/{{cookiecutter.__image_name}}:latest" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/{{cookiecutter.__image_name}}:${CURRENT_BRANCH}" \
+  $buildx_tags \
   --load .
-
