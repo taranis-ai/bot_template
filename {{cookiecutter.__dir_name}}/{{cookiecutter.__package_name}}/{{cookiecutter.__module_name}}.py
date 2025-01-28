e02383b77d35766e2d119d7485949ec39085de3b
llinauer <l.linauer@gmx.at>
2025-01-28 10:24:20 +0100

    Add template for actual bot logic

---
+++ {{cookiecutter.__dir_name}}/{{cookiecutter.__package_name}}/{{cookiecutter.__module_name}}.py
@@ -0,0 +1,9 @@
+class {{cookiecutter.__class_name}}:
+    def __init__(self):
+        # add class attributes here
+        pass
+
+    def predict(self):
+
+        # add inference code here
+        raise NotImplementedError("Bot class {{cookiecutter.__class_name}} must implement 'predict' method")
