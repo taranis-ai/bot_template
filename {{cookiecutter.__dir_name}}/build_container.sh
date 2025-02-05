fbb3ea8ef654856d0f1ab035ea2e9ac6d6ee1a84
llinauer <l.linauer@gmx.at>
2025-02-05 14:57:56 +0100

    Make build_container.sh conditional on whether directory is a git repo

--- {{cookiecutter.__dir_name}}/build_container.sh
+++ {{cookiecutter.__dir_name}}/build_container.sh
@@ -9,23 +9,46 @@ fi
 
 set -eou pipefail
 
-cd $(git rev-parse --show-toplevel)
+build_tags=""
+build_args=""
 
-GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY_OWNER:-"ghcr.io/taranis-ai"}
-buildx_tags="--tag ${GITHUB_REPOSITORY_OWNER}/taranis-awesomebot:latest "
 INCLUDED_MODEL=${INCLUDED_MODEL:-}
 
-if git rev-parse --quiet --verify HEAD >/dev/null; then
-    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
-    echo "Building image for branch ${CURRENT_BRANCH} on ${GITHUB_REPOSITORY_OWNER}"
-    buildx_tags+="--tag ${GITHUB_REPOSITORY_OWNER}/taranis-awesomebot:${CURRENT_BRANCH}"
-else
-    echo "The repository has no commits yet. Building image locally"
-fi
+# if not a git repo, build locally
+if [ -d .git ]; then
+    cd $(git rev-parse --show-toplevel)
 
+    repo_name=$(grep 'url =' .git/config | sed -E 's/.*[:\/]([^\/]+)\.git/\1/')
+    # if remote url for repo not given -> abort
+    if [ -z "$repo_name" ]; then
+        echo "Error: Could not get remote repository name. Please check your .git/config"
+        exit 1
+    fi
+
+    GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY_OWNER:-"ghcr.io/taranis-ai"}
+    build_args+="--build-arg GITHUB_REPOSITORY_OWNER='${GITHUB_REPOSITORY_OWNER}'"
+    build_tags+="--tag ${GITHUB_REPOSITORY_OWNER}/${REPO_NAME}>:latest "
+    build_tags+="--tag '${GITHUB_REPOSITORY_OWNER}/${REPO_NAME}:${INCLUDED_MODEL}'"
+
+    # check if there are any commits yet
+    if git rev-parse --quiet --verify HEAD >/dev/null; then
+        CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
+        echo "Building image for branch ${CURRENT_BRANCH} and model ${INCLUDED_MODEL} on ${GITHUB_REPOSITORY_OWNER}"
+        build_tags+="--tag ${GITHUB_REPOSITORY_OWNER}/${REPO_NAME}:${CURRENT_BRANCH} "
+    else
+        echo "The repository has no commits yet."
+    fi
+
+else
+    build_tags+="--tag {{cookiecutter.__package_name}}:${INCLUDED_MODEL} "
+    build_tags+="--tag {{cookiecutter.__package_name}}:latest"
+    echo "Current directory is not a git repo. Building image for model ${INCLUDED_MODEL} locally"
+
+fi;
+
+build_args+="--build-arg MODEL='${INCLUDED_MODEL}'"
 
 docker buildx build --file Containerfile \
-  --build-arg GITHUB_REPOSITORY_OWNER="${GITHUB_REPOSITORY_OWNER}" \
-  --build-arg MODEL="${INCLUDED_MODEL}" \
-  $buildx_tags \
+  $build_args \
+  $build_tags \
   --load .
