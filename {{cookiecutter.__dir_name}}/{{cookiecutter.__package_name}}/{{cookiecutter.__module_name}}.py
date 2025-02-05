b453a1054b0a2e135917a10c874e22c0f525d83d
llinauer <l.linauer@gmx.at>
2025-02-05 12:29:43 +0100

    Remove template model file

--- {{cookiecutter.__dir_name}}/{{cookiecutter.__package_name}}/{{cookiecutter.__module_name}}.py
+++
@@ -1,8 +0,0 @@
-class {{cookiecutter.__class_name}}:
-    def __init__(self):
-        # add class attributes here
-        pass
-
-    def predict(self):
-        # add inference code here
-        raise NotImplementedError("Bot class {{cookiecutter.__class_name}} must implement 'predict' method")
