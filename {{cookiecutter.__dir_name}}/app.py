b170db8f3a5a2c6a3e64cfd818c15fa2e89c5ccd
llinauer <l.linauer@gmx.at>
2025-10-22 10:38:47 +0200

    Use taranis-base-bot style app.py

--- {{cookiecutter.__dir_name}}/app.py
+++ {{cookiecutter.__dir_name}}/app.py
@@ -1,3 +1,4 @@
-from {{cookiecutter.__package_name}} import create_app
+from cybersec_classifier.config import Config
+from taranis_base_bot import create_app
 
-app = create_app()
+app = create_app(Config.PACKAGE_NAME, Config)
