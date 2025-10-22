ea343dde08a1aeeeae2046a1d0494664c9b1fd9b
llinauer <l.linauer@gmx.at>
2025-10-22 12:05:15 +0200

    Remove ruff again from pre-commits; messes with cookiecutter

--- {{cookiecutter.__dir_name}}/app.py
+++ {{cookiecutter.__dir_name}}/app.py
@@ -1,4 +1,4 @@
-from cybersec_classifier.config import Config
+from {{cookiecutter.__package_name}}.config import Config
 from taranis_base_bot import create_app
 
 app = create_app(Config.PACKAGE_NAME, Config)
