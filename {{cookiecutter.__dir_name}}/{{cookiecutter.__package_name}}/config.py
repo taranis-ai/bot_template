23ffc09754e982bf865a72b6687f4abfac84d999
llinauer <l.linauer@gmx.at>
2025-10-22 11:04:06 +0200

    Rename PAYLOAD_KEY from "text" -> "key" to make it more general

--- {{cookiecutter.__dir_name}}/{{cookiecutter.__package_name}}/config.py
+++ {{cookiecutter.__dir_name}}/{{cookiecutter.__package_name}}/config.py
@@ -1,11 +1,12 @@
 from typing import Literal
 from taranis_base_bot.config import CommonSettings
 
+
 class Settings(CommonSettings):
     MODEL: Literal[""]
     PACKAGE_NAME: str = "{{cookiecutter.__package_name}}"
     HF_MODEL_INFO: bool = True
-    PAYLOAD_KEY: str = "text"
+    PAYLOAD_KEY: str = "key"
 
 
 Config = Settings()
